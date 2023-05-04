5be57e492933daa9743cf4496738ab191142b938
“gabrielleshieh” <gabrielle_shieh@brown.edu>
2023-05-04 16:27:58 -0400

    small changes

--- model.py
+++ model.py
@@ -14,44 +14,45 @@ class EmotionDetectionModel(tf.keras.Model):
     @tf.function
     def call(self, encoded_text):
         return self.decoder(encoded_text)  
-    
-    def optimizer(self, learning_rate):
-        return tf.keras.optimizers.Adam(learning_rate=learning_rate)
 
     def compile(self, optimizer, loss, metrics):
         '''
         Create a facade to mimic normal keras fit routine
         '''
         self.optimizer = optimizer
-        self.loss_function = loss 
-        self.accuracy_function = metrics[0]
+        self.sentiment_loss = loss 
+        self.accuracy = metrics[0]
+    
+        
+def optimizer(learning_rate):
+    return tf.keras.optimizers.Adam(learning_rate=learning_rate)
 
-    # Model loss function 
-    def sentiment_loss(self, labels, predictions):
-        """Loss function for sentiment analysis"""
-        return tf.keras.losses.sparse_categorical_crossentropy(labels, predictions)
+# Model loss function 
+def sentiment_loss(labels, predictions):
+    """Loss function for sentiment analysis"""
+    return tf.keras.losses.sparse_categorical_crossentropy(labels, predictions)
 
-    def summary_loss(self, labels, predictions, threshold, perplexity):
-        """Loss function"""
-        other_emotions = 0
-        target_emotion = 0
-        for i in predictions:
-            if i >= threshold:
-                target_emotion += i
-            else:
-                other_emotions += i
-        loss = other_emotions + perplexity - target_emotion
-        return loss
+def summary_loss(labels, predictions, threshold, perplexity):
+    """Loss function"""
+    other_emotions = 0
+    target_emotion = 0
+    for i in predictions:
+        if i >= threshold:
+            target_emotion += i
+        else:
+            other_emotions += i
+    loss = other_emotions + perplexity - target_emotion
+    return loss
 
-    def accuracy(self, logits, labels):
-        """Computes accuracy and returns a float representing the average accuracy"""
+def accuracy(logits, labels):
+    """Computes accuracy and returns a float representing the average accuracy"""
 
-        correct_predictions = np.argmax(logits, axis=1)
-        num_correct = 0
+    correct_predictions = np.argmax(logits, axis=1)
+    num_correct = 0
 
-        for prediction, label in zip(list(correct_predictions), list(labels)):
-            if prediction == label: 
-                num_correct += 1
+    for prediction, label in zip(list(correct_predictions), list(labels)):
+        if prediction == label: 
+            num_correct += 1
 
-        avg = num_correct / len(labels)
-        return avg
+    avg = num_correct / len(labels)
+    return avg
